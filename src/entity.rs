0ade876b9e1cf769d9f59c1f19fac9faee1fa77e
ganmin <jxz_211@163.com>
2020-11-20 00:44:35 +0800

    修改通用查找

--- src/entity.rs
+++ src/entity.rs
@@ -8,44 +8,120 @@ use crate::data::ENTITIES;
 use lazy_static::lazy_static;
 use std::collections::HashMap;
 use std::sync::Mutex;
-type SortedEntity = Vec<(Vec<u8>, u32)>;
+use std::cmp::{ Ord, Ordering};
+use std::fmt::{Debug};
+type SortedEntity = Vec<DecodeEntityItem>;
+
+#[derive(Debug, Eq)]
+struct DecodeEntityItem{
+  chars: Vec<u8>,
+  code: u32,
+  entity: &'static str
+}
+
+impl Ord for DecodeEntityItem{
+  fn cmp(&self, other: &Self) -> Ordering {
+      let Self{chars,..} = self;
+      let Self{chars: other_chars, ..} = other;
+      let cur_len = chars.len();
+      let other_len = other_chars.len();
+      let max_len = if cur_len > other_len{
+        cur_len
+      } else {
+        other_len
+      };
+      for index in 0..max_len{
+        let cur_char = chars.get(index).unwrap_or(&0);
+        let other_char = other_chars.get(index).unwrap_or(&0);
+        if cur_char != other_char{
+          return cur_char.cmp(other_char);
+        }
+      }
+      Ordering::Equal
+  }
+}
+impl PartialOrd for DecodeEntityItem{
+  fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+      Some(self.cmp(other))
+  }
+}
+
+impl PartialEq for DecodeEntityItem{
+  fn eq(&self, other: &Self) -> bool {
+      let Self{chars,..} = self;
+      let Self{chars: other_chars, ..} = other; 
+      let len = chars.len();
+      if len == other_chars.len(){
+        for index in 0..len{
+          if chars[index] != other_chars[index]{
+            return false;
+          }
+        }
+        return true;
+      }
+      false
+  }
+}
+
+
 lazy_static! {
   static ref IS_SORTED: Mutex<bool> = Mutex::new(false);
   static ref DECODE_ENTITIES: Mutex<SortedEntity> = Mutex::new(vec![]);
+  static ref FIRST_POSITION: Mutex<HashMap<u8,(u32, u32)>> = Mutex::new(HashMap::new());
 }
 /**
  * Find the charCode index
  * 二分查找法查找当前code
 */
 fn search_char_code_pos(char_code: u32, start_index: usize, end_index: usize) -> Option<usize> {
-  let (_, start_char_code) = ENTITIES[start_index];
-  let (_, end_char_code) = ENTITIES[end_index];
-  if char_code > start_char_code && char_code < end_char_code {
-    let middle_index = (start_index + end_index) / 2;
-    let (_, middle_char_code) = ENTITIES[middle_index];
-    if char_code == middle_char_code {
-      Some(middle_index)
-    } else {
-      if char_code > middle_char_code {
-        if end_index - middle_index > 1 {
-          search_char_code_pos(char_code, middle_index, end_index)
-        } else {
-          None
-        }
-      } else {
-        if middle_index - start_index > 1 {
-          search_char_code_pos(char_code, start_index, middle_index)
-        } else {
-          None
-        }
-      }
-    }
-  } else if char_code == start_char_code {
-    Some(start_index)
-  } else if char_code == end_char_code {
-    Some(end_index)
-  } else {
+  let (index, finded) = binary_search_index(char_code, start_index, end_index, |i|{
+    let (_, code) = &ENTITIES[i];
+    *code
+  });
+  if finded{
+    Some(index)
+  }else{
     None
+  }
+}
+/**
+* 二分查找
+*/
+fn binary_search_index<T: Ord + Debug, F>(compared: T, start_index: usize, end_index: usize, cb: F)-> (usize, bool)
+where F: Fn(usize) -> T{
+  let start_value = cb(start_index);
+  let end_value = cb(end_index);
+  let more_than_start = compared > start_value;
+  if more_than_start && compared < end_value {
+    let middle_index = (start_index + end_index) / 2;
+    let middle_value = cb(middle_index);
+    if compared == middle_value {
+      return (middle_index, true);
+    }
+    if compared > middle_value {
+      if end_index - middle_index > 1{
+        return binary_search_index(compared, middle_index, end_index, cb);
+      }
+      return (end_index, false);
+    } else {
+      if middle_index - start_index > 1 {
+        return binary_search_index(compared, start_index, middle_index, cb);
+      }
+      return (start_index + 1, false);
+    }
+  } 
+  if compared == start_value {
+    return (start_index, true);
+  } 
+  if compared == end_value {
+    return (end_index, true);
+  }
+  if !more_than_start{
+    // less than start
+    (start_index, false)
+  } else {
+    // more than start, but also more than end
+    (end_index + 1, false)
   }
 }
 /**
@@ -87,26 +163,66 @@ pub fn encode(content: &str) -> (String, u32) {
  * 将entities排序成所需格式
  */
 fn sort_entities() {
-  let result: SortedEntity = Vec::with_capacity(ENTITIES.len());
-  let counts: HashMap<u8, u32> = HashMap::new();
-  let firsts: Vec<u8> = Vec::with_capacity(52);
+  let mut sorted: SortedEntity = Vec::with_capacity(ENTITIES.len());
+  let mut counts: HashMap<u8, (usize, usize)> = HashMap::new();
+  let mut firsts: Vec<u8> = Vec::with_capacity(52);
   // 二分查找插入
   for pair in &ENTITIES[..] {
     let (entity, code) = pair;
-    let entity = entity.as_bytes();
-    let entity = (&entity[1..entity.len() - 1]).to_vec();
-    let first = entity[0];
+    let chars = entity.as_bytes();
+    let chars = (&chars[1..chars.len() - 1]).to_vec();
+    let first = chars[0];
+    let entity = DecodeEntityItem{
+      chars,
+      code: *code,
+      entity
+    };
+    binary_insert(&mut sorted, entity);
+    // 插入首字母个数到hashmap
+    match counts.get_mut(&first) {
+      Some((v, _)) => {
+        *v += 1;
+      }
+      None => {
+        counts.insert(first, (1, 0));
+      }
+    }
+    // 插入首字母到表
+    if !firsts.contains(&first){
+      firsts.push(first);
+    }
   }
+  // 整理首位序号
+  firsts.sort_unstable();
+  let mut cur_index:usize = 0;
+  for char_code in firsts{
+    let position = counts.get_mut(&char_code).unwrap();
+    let next_index = cur_index + position.0;
+    *position = (cur_index, next_index);
+    cur_index = next_index;
+  }
+
+  println!("counts:{:?}", counts);
 }
 /**
  * 二分查找插入
  */
-fn binary_insert() {}
+fn binary_insert(sorted: &mut SortedEntity, cur: DecodeEntityItem) {
+  let mut prev_index = 0;
+  let len = sorted.len();
+  if len > 0{
+    let (index, _) = binary_search_index(&cur, 0, len - 1, |i|{
+      &sorted[i]
+    });
+    prev_index = index;
+  }
+  (*sorted).insert(prev_index, cur);
+}
 /**
  * Decode
  * 将html实体转化为具体字符
  */
-pub fn decode(content: &str) {
+pub fn decode(_content: &str) {
   let mut is_sorted = IS_SORTED.lock().unwrap();
   if !*is_sorted {
     sort_entities();
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use htmlentity::entity::encode;
+use htmlentity::entity::{encode, decode};
 fn main() {
-    let ch = "\t
-        abhaha&agsdgs&\"this is a test
-    ";
-    let result = encode(ch);
-    println!("result is {:?}", result);
+    // let ch = "\t
+    //     abhaha&agsdgs&\"this is a test
+    // ";
+    // let result = encode(ch);
+    // println!("result is {:?}", result);
+    decode("haha&nbsp;")
 }
