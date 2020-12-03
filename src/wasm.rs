909a4aa4b1d41c2f13f230b781d602666d27ed79
ganmin <ganmin@360.cn>
2020-12-03 15:06:38 +0800

    fix: fix wasm wrong variable name

--- src/wasm.rs
+++ src/wasm.rs
@@ -1,6 +1,6 @@
 #![cfg(target_arch = "wasm32")]
+use crate::entity::{decode as r_decode, encode as r_encode, EncodeType, EntitySet};
 use wasm_bindgen::prelude::*;
-use crate::entity::{ EncodeType, Entities, encode as r_encode, decode as r_decode };
 
 #[wasm_bindgen]
 extern "C" {
@@ -14,12 +14,20 @@ export type IString = string;
 "#;
 
 #[wasm_bindgen]
-pub fn encode(content: &str, entities: Option<Entities>, encode_type: Option<EncodeType>) -> IString{
-  let result = r_encode(content, entities.unwrap_or_default() , encode_type.unwrap_or_default());
+pub fn encode(
+  content: &str,
+  entities: Option<EntitySet>,
+  encode_type: Option<EncodeType>,
+) -> IString {
+  let result = r_encode(
+    content,
+    entities.unwrap_or_default(),
+    encode_type.unwrap_or_default(),
+  );
   JsValue::from_str(&result).into()
 }
 
 #[wasm_bindgen]
-pub fn decode(content: &str) -> IString{
+pub fn decode(content: &str) -> IString {
   JsValue::from_str(&r_decode(content)).into()
 }
